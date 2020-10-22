e600b0c940a9e86e6fd29a8f243936685ccd7c8c
Jose Luis <jllavado0@gmail.com>
2020-10-22 09:50:07 +0200

    Final version de todo y con comentarios

--- final_crawler.py
+++ final_crawler.py
@@ -3,8 +3,6 @@ import json
 import os
 import pickle
 import time
-from os import listdir
-from os.path import isfile, join
 
 import pandas as pd
 import requests
@@ -12,6 +10,7 @@ from bs4 import BeautifulSoup as bs
 from scrapy import Selector
 
 
+# Remove not alpha characters and no-spaces
 def is_alpha_and_spaces(string):
     return string.replace(' ', '') \
         .replace('-', '') \
@@ -19,15 +18,17 @@ def is_alpha_and_spaces(string):
         .isalpha()
 
 
-def onlyfiles(path, extension=""):
-    return [f.replace(extension, "") for f in listdir(path) if isfile(join(path, f))]
-
-
+# Path donde guardar los datos (csv, htmls, y pickle)
 DATA_PATH = "data"
-# Cambiar por uno mas identificativo
+
+# Crawler ettiquette
 HEADERS = {'User-Agent': 'Custom / crawler para proyecto academico jose.lavado@estudiante.uam.es'}
 
 
+# Funcion para hacer request robustos
+# Reintenta un maximo de max_retries intentos (default 5)
+# Entre intento e intento expera time_between_retries * intentos_que_lleva (default 1, -> 1,2,4,8,16)
+# Devuelve la respuesta (commpleta) o None en caso de no conseguirlo en el numero de intentos especificados
 def request_robusto(link, max_retries=5, time_between_retries=1):
     success = False
     retries = 1
@@ -48,8 +49,13 @@ def request_robusto(link, max_retries=5, time_between_retries=1):
     return request
 
 
+# Scrapping (manual) de la pagina de valores para guardar toda la evoluvcion del jugador posterior a no_before
 def extract_market_value(link, no_before=datetime.datetime(2015, 1, 1)):
     res = request_robusto(link)
+
+    # Usamos beautiful soup 4 por que al ser una grafica (svg) que carga en dinamico necesitamos hacer scrapping
+    # al codigo javascript que la genera que es donde estan los datos realmente
+
     soup = bs(res.content, 'lxml')
     scripts = soup.select('script[type="text/javascript"]')
     scripts = [str(script) for script in scripts]
@@ -77,18 +83,22 @@ def extract_market_value(link, no_before=datetime.datetime(2015, 1, 1)):
     return value_dict
 
 
+# Scrapping (manual) de la tabla con todos los titulos del jugador que sean no anteriores a la temporada 15/16 o el 2015
 def extract_cups(link, no_before="15"):
     res = request_robusto(link)
     sel = Selector(text=res.content)
+    # Cogemos todas la filas de la tabla de titulos
     parent = sel.css('div.large-4 > div.box  tbody tr')
     titulos_dict = dict()
     for element in parent:
         element_classes = element.css('::attr(class)').extract()
         if 'bg_Sturm' in element_classes:
+            # Nombre del titulo
             titulo = element.css('td::text')[0].extract()
             titulo = titulo.split('x')[1].strip()
             titulos_dict[titulo] = []
         else:
+            # Info hay que tener en cuenta que hay muchos tipos diferentes de titulos.
             ano = element.css('td:nth-of-type(1) ::text')[0].extract()
             if '/' in ano:
                 ano_splits = [int(x) for x in ano.split('/')]
@@ -113,6 +123,7 @@ def extract_cups(link, no_before="15"):
             single_cup = {'anno': ano, 'equipo_o_liga': equipo_o_liga, 'extra_info': extra_info}
             titulos_dict[titulo].append(single_cup)
 
+    # Filtrado de tiulos vacios (por que no se ganaron despues de la fecha indicada)
     for key in titulos_dict.copy().keys():
         if len(titulos_dict[key]) == 0:
             del titulos_dict[key]
@@ -131,9 +142,15 @@ if __name__ == '__main__':
     response = request_robusto(most_valuable_players_url)
     sel = Selector(text=response.content)
 
+    # Mirar a ver si hay link a la siguiente pagina
     selector_siguiente = sel.css('li.naechste-seite > a')
-    while True:
+
+    while True:  # Mientras haya siguiente
+        # Coger todas las primeras celdas (el link dentro)
         parent = sel.css('td > a.spielprofil_tooltip')
+
+        # Cogemos de cada jugador de la pagina su nombre (sin caracteres extranos),
+        # el link a su perfil (para sacar luego sus titulos y su evoluvcion), su valor y su edad
 
         nombres = [nombre for nombre in parent.css('::text').extract() if is_alpha_and_spaces(nombre)]
         links = [prefix + link for link in parent.css(' ::attr(href)').extract() if '#' not in link]
@@ -145,16 +162,19 @@ if __name__ == '__main__':
         precios = [precio for precio in parent.css(' ::text').extract()]
 
         for nombre, link, edad, precio in zip(nombres, links, edades, precios):
+            # Por cada jugador en la pagina
             response = request_robusto(link)
             player_sel = Selector(text=response.content)
             nombre_check = ' '.join([x.strip() for x in player_sel.css('div.dataName > h1 ::text').extract()])
             id = int(link.split("/")[-1])
 
+            # Comprobamos que el el link es correcto yendo a su perfil y viendo que los nombres coinciden
             if nombre != nombre_check:
                 print("ERROR")
                 break
             nombre_canonico = link.split('/')[3]
 
+            # Guardamos datos identificativos en un data frame (que luego salvamos en .csv)
             df.loc[len(df.index)] = [id, nombre_canonico, edad, precio]
 
             nombre_fichero = nombre_canonico + "-" + str(id)
@@ -163,11 +183,13 @@ if __name__ == '__main__':
 
             filename = f"{DATA_PATH}/htmls/{nombre_fichero}.html"
 
+            # Guardamos el html completo de su pagina de perfil
             with open(filename, "wb") as f:
                 f.write(response.content)
 
             print(f"Finish {nombre_fichero} html y dataframe")
 
+            # Funcion para extraer el valor a partir del link (modificado) del perfil
             link_value = link.replace('profil', 'marktwertverlauf')
             value_dict = extract_market_value(link_value)
 
@@ -175,18 +197,21 @@ if __name__ == '__main__':
 
             filename = f"{DATA_PATH}/values/{nombre_fichero}.pickle"
 
+            # Salvamos diciconario con la evolucion del valor (formato pickle)
             with open(filename, "wb") as f:
                 pickle.dump(value_dict, f)
 
             print(f"Finish {nombre_fichero} value")
 
-            link_cups= link.replace('profil', 'erfolge')
+            # Funcion para extraer los titulos a partir del link (modificado) del perfil
+            link_cups = link.replace('profil', 'erfolge')
             cups_dict = extract_cups(link_cups)
 
             os.makedirs(f"{DATA_PATH}/cups/", exist_ok=True)
 
             filename = f"{DATA_PATH}/cups/{nombre_fichero}.pickle"
 
+            # Salvamos diciconario con todos los titulos (formato pickle)
             with open(filename, "wb") as f:
                 pickle.dump(cups_dict, f)
 
@@ -194,9 +219,16 @@ if __name__ == '__main__':
 
         selector_siguiente = sel.css('li.naechste-seite > a')
 
+        # Si no  hay siguiente pagina hemos terminado 25 jugadores x 20 paginas = 500 jugadores
         if len(selector_siguiente) == 0:
             break
 
+        # Link pagina siguiente
         siguiente_link = prefix + selector_siguiente.css('::attr(href)').extract()[0]
+
+        # Cargamos pagina siguiente (y volvemos a empezar) -> seria equivalente a ampliar la frontera
         response = request_robusto(siguiente_link)
         sel = Selector(text=response.content)
+
+    # Save pandas dataframe to csv
+    df.to_csv(f'{DATA_PATH}/Raw_All_Players_Data.csv')
